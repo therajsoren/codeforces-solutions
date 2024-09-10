5f6a4b6806f87f5a9636ae6d92338d5d52a615ce
Raj Soren <68795729+therajsoren@users.noreply.github.com>
2024-09-10 22:21:50 +0000

    added new file

---
+++ 900/1374B. Multiply by 2, divide by 6.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n; cin >> n;
+    int cnt = 0;
+    while (n != 1) {
+        if (n % 6 == 0) n /= 6;
+        else if (n % 3 == 0) n *= 2; // if n is not multipy by 6
+        else {
+            cout << -1 << endl;
+            return;
+        }
+        cnt++;
+    }
+    cout << cnt << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int tt = 1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+}
